2563590b89790050492023e18b526ff2deb5630c
jonathandurey <56510426+jonathandurey@users.noreply.github.com>
2019-10-13 09:27:10 -0500

    Add files via upload

---
+++ EBBWebApi/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EBBModels.Models;
+using Newtonsoft.Json;
+using EBBDataServices.Implementations;
+using EBBDataServices.Interfaces;
+namespace EBBWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        IUserService userService = new UserSerivce();        
+       
+        [HttpPost]
+        [Route("UpdateLoginInfo")]
+        public string UpdateLoginInfo([FromBody] LoginViewModel model)
+        {
+            var resp = userService.UpdateLoginInfo(model);
+            return JsonConvert.SerializeObject(resp, Formatting.None);
+
+        }
+
+        [HttpGet, Route("GetUserByCredentials/{username}/{password}")]
+        public string GetUserByCredentials(string username, string password)
+        {
+            var model = userService.GetUserByCredentials(username, password);
+            return JsonConvert.SerializeObject(model, Formatting.None); 
+        }     
+    }
+}
---
+++ EBBWebApi/Controllers/NominationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EBBModels.Models;
+using Newtonsoft.Json;
+using EBBDataServices.Implementations;
+using EBBDataServices.Interfaces;
+namespace EBBWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NominationController : ControllerBase
+    {
+        INominationService _nominationService = new NominationService();
+
+        [HttpGet, Route("GetPackage/{id}")]
+        public string GetPackage(string id)
+        {
+            var obj = _nominationService.GetPackage(id);
+            return JsonConvert.SerializeObject(obj, Formatting.None);
+        }
+
+        [HttpPost, Route("SavePackage")]
+        public string SavePackage([FromBody] PackageViewModel model)
+        {
+            var obj = _nominationService.SavePackage(model);
+            return JsonConvert.SerializeObject(obj, Formatting.None);
+        }
+    }
+}
